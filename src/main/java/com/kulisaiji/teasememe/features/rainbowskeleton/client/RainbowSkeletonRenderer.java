2495899956d5ce4d7f3fc5af423325db4d9de6e0
kulisaihu <q2034384307@163.com>
2026-02-27 03:44:30 +0000

    fix: 修复 BipedEntityRenderer 模型构造方式
    使用 getPart() 方法获取模型部分，确保兼容 Minecraft 1.21.1 API
    Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- src/main/java/com/kulisaiji/teasememe/features/rainbowskeleton/client/RainbowSkeletonRenderer.java
+++ src/main/java/com/kulisaiji/teasememe/features/rainbowskeleton/client/RainbowSkeletonRenderer.java
@@ -4,10 +4,10 @@ import com.kulisaiji.teasememe.TeaseMemeMod;
 import com.kulisaiji.teasememe.features.rainbowskeleton.entity.RainbowSkeletonEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
@@ -16,8 +16,13 @@ public class RainbowSkeletonRenderer extends BipedEntityRenderer<RainbowSkeleton
     private static final Identifier TEXTURE = Identifier.of(TeaseMemeMod.MOD_ID, "textures/entity/skeleton_glow.png");
 
     public RainbowSkeletonRenderer(EntityRendererFactory.Context context) {
-        super(context, new BipedEntityModel<>(context.getModelLoader().getModelPart(EntityModelLayers.SKELETON)), 0.5f);
+        super(context, createModel(context), 0.5f);
         this.addLayer(new RainbowGlowFeature<>(this));
+    }
+
+    private static BipedEntityModel<RainbowSkeletonEntity> createModel(EntityRendererFactory.Context context) {
+        ModelPart root = context.getModelLoader().getPart(context.getModelLoader().getModelRegistry().get(net.minecraft.client.render.entity.model.EntityModelLayers.SKELETON));
+        return new BipedEntityModel<>(root);
     }
 
     @Override
